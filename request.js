d910ab3f27faa81a91005ba6ef5d07d95df82a20
d4t06 <huudat01234560@gmail.com>
2024-05-14 07:41:16 +0700

    add time out

--- request.js
+++ request.js
@@ -1,21 +1,33 @@
+const timeOut = async (delay = 1000 * 60) =>
+   new Promise((rs, rj) =>
+      setTimeout(() => {
+         rj("time out");
+      }, delay)
+   );
+
 const sleep = async (delay = 1000 * 60) =>
-   new Promise((rs) => setTimeout(() => rs(), delay));
+   new Promise((rs) =>
+      setTimeout(() => {
+         rs();
+      }, delay)
+   );
 
 const makeRequest = async () => {
    return Promise.all([
-      fetch("https://chat-app-backend-latest.onrender.com/", {}),
+      fetch("https://chat-app-backend-latest.onrender.com/"),
       fetch("https://spring-mobile-latest.onrender.com/api/"),
    ]);
 };
 
 const run = async () => {
    try {
-      await makeRequest();
-      console.log('successful');
+      console.log("request");
+      await Promise.race([makeRequest(), timeOut(5000)]);
    } catch (error) {
       console.log({ message: error });
    } finally {
-      await sleep(1000 * 60 * 2);
+      console.log("finally");
+      await sleep(1000 * 30);
       run();
    }
 };
